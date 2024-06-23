e80f71b95b2a79754bd88f7072d2c99fd79a7eea
croko22 <kvchambit@gmail.com>
2024-06-23 18:40:41 -0500

    refactor: Update routes/web.php for authentication and authorization

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', [MovieController::class, 'index'])->name('home');
+    Route::resource('movies', MovieController::class);
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', function () {
+        return view('login');
+    });
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::get('/register', function () {
+        return view('register');
+    });
+    Route::post('/register', [AuthController::class, 'login'])->name('register');
 });
