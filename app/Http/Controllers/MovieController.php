d0b495861550b75d9d67d60fa26ef964e49ff9cc
croko22 <kvchambit@gmail.com>
2024-06-25 08:51:53 -0500

    Query by title, genre, director and year

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Movie;
+use App\Models\Genre;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
@@ -10,11 +11,34 @@ class MovieController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $movies = Movie::all();
-        // dd($movies);
-        return view('home', compact('movies'));
+        $search = $request->input("search");
+        $genres = $request->input("genres", []); // Adjusted for multiple genres
+        $director = $request->input("director");
+        $year = $request->input("year");
+        $allGenres = Genre::all();
+
+        $movies = Movie::query()
+            ->when($search, function ($query, $search) {
+                return $query->where('name', 'like', "%$search%");
+            })
+            ->when($genres, function ($query, $genres) {
+                return $query->whereHas('genres', function ($query) use ($genres) {
+                    $query->whereIn('genres.id', $genres);
+                });
+            })
+            ->when($director, function ($query, $director) {
+                return $query->whereHas('director', function ($query) use ($director) {
+                    $query->where('name', 'like', "%$director%");
+                });
+            })
+            ->when($year, function ($query, $year) {
+                return $query->where('year', '=', $year);
+            })
+            ->paginate(12)->withQueryString();
+
+        return view('home', compact('movies', 'allGenres'));
     }
 
     /**
