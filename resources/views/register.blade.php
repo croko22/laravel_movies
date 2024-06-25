a7066279722cb18993ab1d7c4e7cb0ebb2bdd177
croko22 <kvchambit@gmail.com>
2024-06-25 08:53:54 -0500

    Error handling

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,6 +2,7 @@
     <h1>LARA MOVIES</h1>
 
     <h2 class="text-2xl font-semibold text-gray-900 dark:text-white">Register</h2>
+
     <form class="max-w-sm mx-auto" action={{ route('register') }} method="POST">
         @csrf
         <div class="mb-5">
@@ -9,6 +10,9 @@
             <input type="text" name="name" id="name"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="John Doe" required />
+            @error('name')
+                <span class="text-red-500">{{ $message }}</span>
+            @enderror
         </div>
         <div class="mb-5">
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
@@ -16,6 +20,9 @@
             <input type="email" id="email" name="email"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="name@flowbite.com" required />
+            @error('email')
+                <span class="text-red-500">{{ $message }}</span>
+            @enderror
         </div>
         <div class="mb-5">
             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
@@ -23,8 +30,12 @@
             <input type="password" id="password" name="password"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required />
+            @error('password')
+                <span class="text-red-500">{{ $message }}</span>
+            @enderror
         </div>
         <button type="submit"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
     </form>
+    <a href={{ route('login') }} class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Login</a>
 </x-layout>
