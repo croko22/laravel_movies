6e68eff229869eb63e9e1c12c26c7cb7f0e578ec
croko22 <kvchambit@gmail.com>
2024-06-25 08:52:13 -0500

    Mail verification

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MovieController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
@@ -18,5 +19,17 @@ Route::middleware(['guest'])->group(function () {
     Route::get('/register', function () {
         return view('register');
     });
-    Route::post('/register', [AuthController::class, 'login'])->name('register');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
 });
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
