e0a673675d48361d2ace0bd4b278ed935877120d
Kevin <kvchambit@gmail.com>
2024-07-02 10:03:37 -0500

    refactor: Improve show view on routes

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', [MovieController::class, 'index'])->name('home');
-    Route::resource('movies', MovieController::class);
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
 Route::middleware(['guest'])->group(function () {
-    Route::get('/login', function () {
-        return view('login');
-    });
+    Route::view('/login', 'login')->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::get('/register', function () {
-        return view('register');
-    });
+    Route::view('/register', 'register')->name('register');
     Route::post('/register', [AuthController::class, 'register'])->name('register');
 });
 
